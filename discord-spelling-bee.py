afeb4c0f8314cfe6f93629501d258facb5e165ad
Daniel Friedman <friedmandaniel111@gmail.com>
2025-06-26 12:29:54 -0700

    Fix time type

--- discord-spelling-bee.py
+++ discord-spelling-bee.py
@@ -13,7 +13,7 @@ dotenv.load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 if not DISCORD_TOKEN:
     raise ValueError("DISCORD_TOKEN environment variable not set.")
-UTC_TIME = os.getenv("UTC_TIME", 0)
+UTC_TIME = os.getenv("UTC_TIME", "0")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -76,7 +76,7 @@ async def today(ctx):
     except Exception as e:
         await ctx.respond("An error occurred while fetching today's stats.", ephemeral=True)
 
-@tasks.loop(time=datetime.time(hour=UTC_TIME))
+@tasks.loop(time=datetime.time(hour=int(UTC_TIME)))
 async def start_games():
     # Fetch Spelling Bee data
     print("Fetching Spelling Bee data...")
